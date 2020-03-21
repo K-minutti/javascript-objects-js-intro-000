a8643b17a0507cc28837ca47afb5680b9314d745
Kevin Minutti <minutti70@gmail.com>
2020-03-21 17:53:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {artistName: "songTitle"}
 
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign( playlist, {[artistName]: songTitle})
+  return Object.assign(playlist, {[artistName]: songTitle})
 }
 
 
