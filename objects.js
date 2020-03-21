5f731a9f6229f03d6c9720584027e8f181148afc
Kevin Minutti <minutti70@gmail.com>
2020-03-21 17:29:47 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+
+Assign an object to the variable playlist and initialize the object with a key-value pair — the keys will be artist names and the values will be song titles. (What limitation does this impose on our playlist?)
+
+
+var playlist
+
+var meals = { breakfast: "oatmeal" };
+
+// or, equivalently
+
+var meals = new Object({ breakfast: 'oatmeal' })
+
+var playlist = {artistNames: "song titles"}
